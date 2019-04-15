1ef83b3ce47bd51e07caa2174583fbce180b1c26
maxhexiaomin_i <maxhexiaomin_i@didichuxing.com>
2019-04-15 10:06:05 +0800

    add patterns

--- src/test.go
+++ src/test.go
@@ -1,21 +1,11 @@
-// Copyright 2019 didi. All rights reserved.
-// Created by hexiaomin on 2019/3/20.
-
 package main
 
-import "errors"
-
 func main(){
-if err := test(); err != nil {
- print(err.Error())
-}
 }
 
+type Father struct {
 
-func test() error{
- var err error
- defer func(er *error) {
-  *er = errors.New("this have error")
- }(&err)
- return err
+}
+type Children struct {
+ father
 }
