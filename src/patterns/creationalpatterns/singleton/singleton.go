cc90015f5569250d7f48627330588a8a1d45bb71
hxmqd <hexiaominqd@163.com>
2019-04-20 00:48:54 +0800

    add

--- src/patterns/creationalpatterns/singleton/singleton.go
+++ src/patterns/creationalpatterns/singleton/singleton.go
@@ -3,22 +3,30 @@ package singleton
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
+	"time"
 )
 
 type Single struct {
+	i int
 }
 
 func (s *Single) ShowMessage() {
-	fmt.Println("hello world!")
+	fmt.Println("hello world!","i=",s.i)
+}
+
+func newSingle(i int) *Single{
+	time.Sleep(time.Duration(rand.Intn(100)))
+	return &Single{i}
 }
 
 // 懒汉式，协程不安全
 var single1 *Single
 
-func GetInstance1() *Single{
+func GetInstance1(i int) *Single{
 	if single1 == nil {
-		single1 = new(Single)
+		single1 = newSingle(i)
 	}
 	return single1
 }
@@ -26,10 +34,10 @@ func GetInstance1() *Single{
 // 懒汉式，协程安全
 var single2 *Single
 var mux sync.Mutex
-func GetInstance2() *Single {
+func GetInstance2(i int) *Single {
 	mux.Lock()
 	if single1 == nil {
-		single1 = new(Single)
+		single1 = newSingle(i)
 	}
 	mux.Unlock()
 	return single1
@@ -44,11 +52,11 @@ func GetInstance2() *Single {
  // 双检锁/双重校验锁(double-checked locking)
 var single4 *Single
 var mux1 sync.Mutex
-func GetInstance4() *Single {
+func GetInstance4(i int) *Single {
 	if single4 == nil {
 		mux1.Lock()
 		if single4 == nil {
-			single4 = new(Single)
+			single4 = newSingle(i)
 		}
 		mux1.Unlock()
 	}
